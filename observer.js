c348354f55cdbcbe90f98d03c333da34e2abdac2
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-04-25 08:36:42 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/Observer@26562 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- observer.js
+++ observer.js
@@ -313,11 +313,6 @@ var Observer = Observer || (function() {
     }
   }
 
-
-
-
-
-
   //  ____  _                                  
   // / __ \| |                                 
   //| |  | | |__  ___  ___ _ ____   _____ _ __ 
@@ -325,7 +320,65 @@ var Observer = Observer || (function() {
   //| |__| | |_) \__ \  __/ |   \ V /  __/ |   
   // \____/|_.__/|___/\___|_|    \_/ \___|_|   
 
+  //  ___  _                            ___                 
+  // / _ \| |__ ___ ___ _ ___ _____ _ _| __|_ _ _ _ ___ _ _ 
+  //| (_) | '_ (_-</ -_) '_\ V / -_) '_| _|| '_| '_/ _ \ '_|
+  // \___/|_.__/__/\___|_|  \_/\___|_| |___|_| |_| \___/_|  
 
+  function ObserverError(message) {
+    this.name = 'Obsever Error';
+    this.message = message || 'Observer constraint violation.';
+    this.stack = (new Error()).stack;
+  }
+  ObserverError.prototype = Object.create(Error.prototype);
+  ObserverError.prototype.constructor = ObserverError;
+
+  //         _ _ _                 
+  // __ __ _| | | |_ _ _ __ _ _ __ 
+  /// _/ _` | | |  _| '_/ _` | '_ \
+  //\__\__,_|_|_|\__|_| \__,_| .__/
+  //                         |_|   
+
+  function calltrap(trap, default, argumentsList) {
+
+    /**
+     * Trap return.
+     * (Default return is undefined.)
+     **/
+    var result = undefined;
+
+    trap.call(this, ...sandbox(argumentsList), function(continuation, ...argumentsList) {
+
+      /**
+       * Checks if arguments are identical.
+       * TODO, is this correct or do we need a new structural equality test 
+       * as the arguments, list may be a new array. 
+       **/
+      //for(var i in argumentsListX) {
+      //  if(argumentsListX[i] === argumentsList) continue;
+      //  else throw new ObserverError();
+      //}
+
+      // TODO
+      // - unwrap arguments/ deactivate membrane
+      // - check if values are identical 
+      // (this requires that all transparent proxies are observer)
+
+      result = default.apply(this, argumentsList);
+
+
+      continuation.call(this, result, function(result) {
+        result = result;
+      });
+    });
+
+
+    /**
+     *
+     **/
+    return result;
+
+  }
 
   // _  _              _ _         
   //| || |__ _ _ _  __| | |___ _ _ 
@@ -333,394 +386,148 @@ var Observer = Observer || (function() {
   //|_||_\__,_|_||_\__,_|_\___|_|  
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-  // _  _              _ _         
-  //| || |__ _ _ _  __| | |___ _ _ 
-  //| __ / _` | ' \/ _` | / -_) '_|
-  //|_||_\__,_|_||_\__,_|_\___|_|  
-
-  function Handler(handler) {
-    if(!(this instanceof Handler)) return new Handler(handler);
-
-    /**
-     * A trap for Object.getPrototypeOf.
-     **/
-    this.getPrototypeOf = function(target) {
-      throw new Error("Trap not implemented."); // TODO
-      //return wrap(Object.getPrototypeOf(target));
-    }
-
-    /**
-     * A trap for Object.setPrototypeOf.
-     **/
-    this.setPrototypeOf = function(target, prototype) {
-      throw new Error("Trap not implemented."); // TODO
-      //throw new MembraneError();
-    }
-
-    /**
-     * A trap for Object.isExtensible
-     **/
-    this.isExtensible = function(target) {
-      throw new Error("Trap not implemented."); // TODO
-      //return Object.isExtensible(target);
-    };
-
-    /** 
-     * A trap for Object.preventExtensions.
-     **/
-    this.preventExtensions = function(target) {
-      throw new Error("Trap not implemented."); // TODO
-      //throw new MembraneError();
-    };
-
-    /** 
-     * A trap for Object.getOwnPropertyDescriptor.
-     **/
-    this.getOwnPropertyDescriptor = function(target, name) {
-      throw new Error("Trap not implemented."); // TODO
-      //return wrap(Object.getOwnPropertyDescriptor(target, name));
-    };
-
-    /** 
-     * A trap for Object.defineProperty.
-     **/
-    this.defineProperty = function(target, name, desc) {
-      throw new Error("Trap not implemented."); // TODO
-      //throw new MembraneError();
-    };
-
-    /** 
-     * A trap for the in operator.
-     **/
-    this.has = function(target, name) {
-      throw new Error("Trap not implemented."); // TODO
-      //return (name in target);
-    };
+  function ObserveHandler(handler) {
 
     /**
      * A trap for getting property values.
-     **/
-    this.get = function(target, name, receiver) {
-
-      // get user specific trap
-      var trap = handler.get;
-
-      // call trap to notify the handler 
-      // and to receive the continuation function
-      var continuation = trap.call(this, sandbox(target), sandbox(name), sandbox(receiver));
-
-      // default operation
-      var result = target[name];
-
-
-      continuation(wrap(result));
-
-
-
-      // call trap to notify the handler 
-      // and to receive the continuation function
-      var continuation = calltrap(handler.get, [target, name, receiver]);
-
-      // default operation
-      var result = target[name];
-
-
-      var continuation 
-
-        call continuation
-
-
-
-
-        var continuation = calltrap(
-            handler.get,
-            default.get,
-            [target, name, receiver]
-            );
-
-
-
-      function observe(handler) {
-        return wrap(result)
-      }
-
-
-
-      function calltrap(trap, default, args) {
-        // frist trap alaso needs teh warp function wo restrict aruments 
-        //
-
-        var continuation = trap.call(this, ...sandbox(args), realm);
-
-        var result = default.apply(this, sandbox(args));
-
-        var resultX = continuation(sandbox(result), ret, obs);
-
-        if()
-
-
-
-
-      }
-
-
-
-
-
-
-
-      /** Begin: default behavior
-       **/
-      var ret = target[name];
-
-      return callTrap('preGet', {return:ret}).return;
-
-
-
-      -
-        -      var configuration = new Configation.Get(targeet, name, receiver);
-      -      
-        -      if(var trap = 'preGet' in handler) handler[trap](configuration);
-      -
-        -
-        -      Configation.equals(configuration, )
-        -
-        -      var {targeet:targeet, name:name, receiver:receiver} = calltrap('get', {targeet:targeet, name:name, receiver:receiver});
-      -
-        -      var {return:return} = 
-        -
-        -
-        -      if(('preGet') in handler) {
-          -        var configuration = {targeet:targeet, name:name, receiver:receiver};
-          -        var configurationP = handler['preGet'](configuration);
-          +      var desc = Object.getOwnPropertyDescriptor(origin, name);
-          +      if(desc && desc.get) {
-            +        var getter = wrap(desc.get);
-            +        return getter.apply(this);
-            +      } else {
-              +        return wrap(target[name])
-            }
-          -
-            -      var 
-            -
-
-            if()handler
-
-
-              if(name === Symbol.toPrimitive) return wrap(origin[name]);
-
-          var desc = Object.getOwnPropertyDescriptor(origin, name);
-          if(desc && desc.get) {
-            var getter = wrap(desc.get);
-            return getter.apply(this);
-          } else {
-            return wrap(target[name])
-          }
-        };
+     * (Meta-Handler only recognizes get calls for traps.)
+     **/ 
+    this.get = function(default, trap, receiver) {
 
       /** 
-       * A trap for setting property values.
+       * If the user-defined handler contains a trap-function for operation <trap>,
+       * then the meta-handler returns a mediator function to catch the trap's arguments 
+       * and calls to call the user-defined trap.
+       * 
+       * Otherwise, the meta-handler returns the default behaviour.
        **/
-      this.set = function(target, name, value, receiver) {
-        throw new Error("Trap not implemented."); // TODO
-        //throw new MembraneError();
-      };
+      return (trap in handler) ? function () {
+        return calltrap(handler[trap], default[trap], arguments)     
+      } ? default[trap];
 
-      /**
-       * A trap for the delete operator.
-       **/
-      this.deleteProperty = function(target, name) {
-        throw new Error("Trap not implemented."); // TODO
-        //throw new MembraneError();
-      };
+    }
+  }
 
-      /** 
-       * A trap for for...in statements.
-       **/
-      this.enumerate = function(target) {
-        throw new Error("Trap not implemented."); // TODO
-        //var properties = new Set();
-        //for(var property in target) {
-        // properties.add(property);
-        //}
-        //return Array.from(properties)[Symbol.iterator]();
-      };
+  // __  __      _          ___  _                            
+  //|  \/  |__ _| |_____   / _ \| |__ ___ ___ _ ___ _____ _ _ 
+  //| |\/| / _` | / / -_) | (_) | '_ (_-</ -_) '_\ V / -_) '_|
+  //|_|  |_\__,_|_\_\___|  \___/|_.__/__/\___|_|  \_/\___|_|  
 
-      /**
-       * A trap for Object.getOwnPropertyNames.
-       **/
-      this.ownKeys = function(target) {
-        throw new Error("Trap not implemented."); // TODO
-        //return Object.getOwnPropertyNames(target);
-      };
+  function mkObserver(realm) {
 
-      /** 
-       * A trap for a function call.
-       **/
-      this.apply = function(target, thisArg, argumentsList) {
-        throw new Error("Trap not implemented."); // TODO
-        //if(target instanceof Pure) {
-        //  return target.apply(wrap(thisArg), wrap(argumentsList))
-        //} else {
-        //  throw new MembraneError();
-        //}
-      };
+    // cache for remembering observer proxies
+    var observers = new realm.WeakSet();
 
-      /** 
-       * A trap for the new operator. 
-       **/
-      this.construct = function(target, argumentsList) {
-        throw new Error("Trap not implemented."); // TODO
-        //if(target instanceof Pure) {
-        //  var thisArg = Object.create(target.prototype);
-        //  var result = target.apply(wrap(thisArg), wrap(argumentsList));
-        //  return (result instanceof Object) ? result : wrap(thisArg);
-        //} else {
-        //  throw new MembraneError();
-        // }
-      }
+
+
+
+
+    //  ___  _                            
+    // / _ \| |__ ___ ___ _ ___ _____ _ _ 
+    //| (_) | '_ (_-</ -_) '_\ V / -_) '_|
+    // \___/|_.__/__/\___|_|  \_/\___|_|  
+
+    function Observer(target, handler, keep=true) {
+
+      // Proxy Constructor
+      var Proxy = realm.Proxy;
+
+      // create new observer based on the given  handler
+      var proxy = new Proxy(target, new ObserverHandler(handler));
+
+      // remembers existing observers
+      if(keep) observers.add(oproxy);
+
+      // return new observer proxy
+      return proxy;
     }
 
+    //                 _       ___          _       
+    // __ _ _ ___ __ _| |_ ___| _ \___ __ _| |_ __  
+    /// _| '_/ -_) _` |  _/ -_)   / -_) _` | | '  \ 
+    //\__|_| \___\__,_|\__\___|_|_\___\__,_|_|_|_|_|
 
+    Object.defineProperty(Observer.prototype, "createRealm", {
+      get: function() {
 
+        // create a new indetitity realm
+        var realm = TransparentProxy.createRealm();
 
+        // redefine toString of new realm
+        Object.defineProperty(realm, "toString", {
+          value: "[[Identitiy Realm]]"
+        });
 
+        // redefine proxy constructor of new realm
+        Object.defineProperty(realm, "Proxy", {
+          value: mkObserver(realm);
+        });
 
-
-
-
-    // __  __      _          ___  _                            
-    //|  \/  |__ _| |_____   / _ \| |__ ___ ___ _ ___ _____ _ _ 
-    //| |\/| / _` | / / -_) | (_) | '_ (_-</ -_) '_\ V / -_) '_|
-    //|_|  |_\__,_|_\_\___|  \___/|_.__/__/\___|_|  \_/\___|_|  
-
-    function mkObserver(realm) {
-
-      // cache for remembering observer proxies
-      var observers = new realm.WeakSet();
-
-
-
-
-
-      //  ___  _                            
-      // / _ \| |__ ___ ___ _ ___ _____ _ _ 
-      //| (_) | '_ (_-</ -_) '_\ V / -_) '_|
-      // \___/|_.__/__/\___|_|  \_/\___|_|  
-
-      function Observer(target, handler, keep=true) {
-
-        // Proxy Constructor
-        var Proxy = realm.Proxy;
-
-        // create new observer based on the given  handler
-        var proxy = new Proxy(target, new ObserverHandler(handler));
-
-        // remembers existing observers
-        if(keep) observers.add(oproxy);
-
-        // return new observer proxy
-        return proxy;
+        // return new realm
+        return realm;
       }
+    });
 
-      //                 _       ___          _       
-      // __ _ _ ___ __ _| |_ ___| _ \___ __ _| |_ __  
-      /// _| '_/ -_) _` |  _/ -_)   / -_) _` | | '  \ 
-      //\__|_| \___\__,_|\__\___|_|_\___\__,_|_|_|_|_|
+    //                _               _           
+    // __ ___ _ _  __| |_ _ _ _  _ __| |_ ___ _ _ 
+    /// _/ _ \ ' \(_-<  _| '_| || / _|  _/ _ \ '_|
+    //\__\___/_||_/__/\__|_|  \_,_\__|\__\___/_|  
 
-      Object.defineProperty(Observer.prototype, "createRealm", {
-        get: function() {
+    Object.defineProperty(Observer.prototype, "constructor", {
+      value: Observer
+    });
 
-          // create a new indetitity realm
-          var realm = TransparentProxy.createRealm();
+    // _       ___ _       _           
+    //| |_ ___/ __| |_ _ _(_)_ _  __ _ 
+    //|  _/ _ \__ \  _| '_| | ' \/ _` |
+    // \__\___/___/\__|_| |_|_||_\__, |
+    //                           |___/ 
 
-          // redefine toString of new realm
-          Object.defineProperty(realm, "toString", {
-            value: "[[Identitiy Realm]]"
-          });
+    Object.defineProperty(Observer, "toString", {
+      value: "[[Obserber Constructor]]"
+    });
 
-          // redefine proxy constructor of new realm
-          Object.defineProperty(realm, "Proxy", {
-            value: mkObserver(realm);
-          });
+    Object.defineProperty(Observer.prototype, "toString", {
+      value: "[[Observer]]"
+    });
 
-          // return new realm
-          return realm;
-        }
-      });
+    //                _          
+    //__ _____ _ _ __(_)___ _ _  
+    //\ V / -_) '_(_-< / _ \ ' \ 
+    // \_/\___|_| /__/_\___/_||_|
 
-      //                _               _           
-      // __ ___ _ _  __| |_ _ _ _  _ __| |_ ___ _ _ 
-      /// _/ _ \ ' \(_-<  _| '_| || / _|  _/ _ \ '_|
-      //\__\___/_||_/__/\__|_|  \_,_\__|\__\___/_|  
+    Object.defineProperty(Observer, "version", {
+      value: "Observer 0.1.0 (PoC)"
+    });
 
-      Object.defineProperty(Observer.prototype, "constructor", {
-        value: Observer
-      });
+    Object.defineProperty(Observer.prototype, "version", {
+      value: Observer.version
+    });
 
-      // _       ___ _       _           
-      //| |_ ___/ __| |_ _ _(_)_ _  __ _ 
-      //|  _/ _ \__ \  _| '_| | ' \/ _` |
-      // \__\___/___/\__|_| |_|_||_\__, |
-      //                           |___/ 
+    // _     ___  _                            
+    //(_)___/ _ \| |__ ___ ___ _ ___ _____ _ _ 
+    //| (_-< (_) | '_ (_-</ -_) '_\ V / -_) '_|
+    //|_/__/\___/|_.__/__/\___|_|  \_/\___|_|  
 
-      Object.defineProperty(Observer, "toString", {
-        value: "[[Obserber Constructor]]"
-      });
-
-      Object.defineProperty(Observer.prototype, "toString", {
-        value: "[[Observer]]"
-      });
-
-      //                _          
-      //__ _____ _ _ __(_)___ _ _  
-      //\ V / -_) '_(_-< / _ \ ' \ 
-      // \_/\___|_| /__/_\___/_||_|
-
-      Object.defineProperty(Observer, "version", {
-        value: "Observer 0.1.0 (PoC)"
-      });
-
-      Object.defineProperty(Observer.prototype, "version", {
-        value: Observer.version
-      });
-
-      // _     ___  _                            
-      //(_)___/ _ \| |__ ___ ___ _ ___ _____ _ _ 
-      //| (_-< (_) | '_ (_-</ -_) '_\ V / -_) '_|
-      //|_/__/\___/|_.__/__/\___|_|  \_/\___|_|  
-
-      Object.defineProperty(Observer, "isObserver", {
-        value: function(object) {
-          return observers.has(object);
-        } 
-      });
-
-      //         _                 
-      // _ _ ___| |_ _  _ _ _ _ _  
-      //| '_/ -_)  _| || | '_| ' \ 
-      //|_| \___|\__|\_,_|_| |_||_|
-
-      return Observer;
-    }
+    Object.defineProperty(Observer, "isObserver", {
+      value: function(object) {
+        return observers.has(object);
+      } 
+    });
 
     //         _                 
     // _ _ ___| |_ _  _ _ _ _ _  
     //| '_/ -_)  _| || | '_| ' \ 
     //|_| \___|\__|\_,_|_| |_||_|
 
-    return mkObserver(TransparentProxy.createRealm());
+    return Observer;
+  }
 
-  })();
+  //         _                 
+  // _ _ ___| |_ _  _ _ _ _ _  
+  //| '_/ -_)  _| || | '_| ' \ 
+  //|_| \___|\__|\_,_|_| |_||_|
+
+  return mkObserver(TransparentProxy.createRealm());
+
+})();
